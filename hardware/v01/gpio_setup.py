b5e46ab03f0941e492eebde8add372951658f339
SMB11 <sgardilyan@gmail.com>
2024-10-13 00:12:35 +0400

    changed to gpiozero

--- hardware/v01/gpio_setup.py
+++ hardware/v01/gpio_setup.py
@@ -1,4 +1,8 @@
 from gpiozero import LED, Button
+from gpiozero.pins.rpigpio import RPiGPIOFactory
+
+# Set up the factory explicitly
+factory = RPiGPIOFactory()
 
 def initialize_gpio():
     global LABELING_START_PIN, LABELING_STOP_PIN, FILLING_STOP_PIN, BLOWING_START_PIN, BLOWING_STOP_PIN
@@ -7,22 +11,22 @@ def initialize_gpio():
     global FILLING_WORKING_PIN, FILLING_ALARM_PIN, FILLING_ALARM_HANDLED_PIN
     global BLOWING_WORKING_PIN, BLOWING_ALARM_PIN
 
-    LABELING_START_PIN = LED(4)
-    LABELING_STOP_PIN = LED(27)
-    FILLING_STOP_PIN = LED(23)
-    BLOWING_START_PIN = LED(24)
-    BLOWING_STOP_PIN = LED(22)
+    LABELING_START_PIN = LED(4, pin_factory=factory)
+    LABELING_STOP_PIN = LED(27, pin_factory=factory)
+    FILLING_STOP_PIN = LED(23, pin_factory=factory)
+    BLOWING_START_PIN = LED(24, pin_factory=factory)
+    BLOWING_STOP_PIN = LED(22, pin_factory=factory)
 
-    SENSOR1_PIN = Button(13)
-    SENSOR2_PIN = Button(26)
-    LABELING_WORKING_PIN = Button(21)
-    LABELING_ALARM_PIN = Button(19)
-    LABELING_ALARM_HANDLED_PIN = Button(12)
-    FILLING_WORKING_PIN = Button(16)
-    FILLING_ALARM_PIN = Button(5)
-    FILLING_ALARM_HANDLED_PIN = Button(6)
-    BLOWING_WORKING_PIN = Button(18)
-    BLOWING_ALARM_PIN = Button(10)
+    SENSOR1_PIN = Button(13, pin_factory=factory)
+    SENSOR2_PIN = Button(26, pin_factory=factory)
+    LABELING_WORKING_PIN = Button(21, pin_factory=factory)
+    LABELING_ALARM_PIN = Button(19, pin_factory=factory)
+    LABELING_ALARM_HANDLED_PIN = Button(12, pin_factory=factory)
+    FILLING_WORKING_PIN = Button(16, pin_factory=factory)
+    FILLING_ALARM_PIN = Button(5, pin_factory=factory)
+    FILLING_ALARM_HANDLED_PIN = Button(6, pin_factory=factory)
+    BLOWING_WORKING_PIN = Button(18, pin_factory=factory)
+    BLOWING_ALARM_PIN = Button(10, pin_factory=factory)
 
 def cleanup_gpio():
     LABELING_START_PIN.close()
